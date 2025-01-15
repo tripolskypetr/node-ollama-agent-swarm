3da4f2b721e9eaba6c96b66d2f420bcddd213b33
Petr Tripolsky <tripolskypetr@gmail.com>
2025-01-15 20:49:24 +0500

    gpt-docs

--- scripts/gpt-docs.mjs
+++ scripts/gpt-docs.mjs
@@ -7,8 +7,11 @@ import fs from "fs";
 
 const MODULE_NAME = "node-ollama-agent-swarm";
 
-const GPT_PROMPT =
+const GPT_CLASS_PROMPT =
   "Please write a summary for that Typescript API Reference with several sentences in more human way";
+
+const GPT_TOTAL_PROMPT =
+  "Please write a summary for the whole system based on API Reference with several sentences in more human way";
 
 console.log("Loading model");
 
@@ -16,20 +19,19 @@ const model = await loadModel("Nous-Hermes-2-Mistral-7B-DPO.Q4_0.gguf", {
     verbose: true,
 });
 
-const generateDescription = async (filePath) => {
+const generateDescription = async (filePath, prompt) => {
     console.log(`Generating content for ${resolve(filePath)}`);
     console.time("EXECUTE");
     const data = fs.readFileSync(filePath).toString();
     const chat = await model.createChatSession({
         temperature: 0.8,
-        systemPrompt: `### System:\n${GPT_PROMPT}.\n\n`,
+        systemPrompt: `### System:\n${prompt}.\n\n`,
     });
     const result = await createCompletion(chat, data);
     console.timeEnd("EXECUTE");
     return result.choices[0].message.content;
 }
 
-// Modules
 {
     const classList = globSync(`./docs/classes/*`);
     const outputPath = join(process.cwd(), 'docs', `${MODULE_NAME}.md`);
@@ -43,8 +45,15 @@ const generateDescription = async (filePath) => {
         const className = basename(classPath, extname(classPath));
         output.push(`## ${className}`);
         output.push("");
-        output.push(await generateDescription(classPath))
+        output.push(await generateDescription(classPath, GPT_CLASS_PROMPT))
         output.push("");
         fs.writeFileSync(outputPath, output.join("\n"));
     }
+    output.shift();
+    output.shift();
+    output.unshift("");
+    output.unshift(await generateDescription(outputPath, GPT_TOTAL_PROMPT));
+    output.unshift("");
+    output.unshift(`# ${MODULE_NAME}`);
+    fs.writeFileSync(outputPath, output.join("\n"));
 }
