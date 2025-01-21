2371521937111f3aa1aa4498750353af27078155
Petr Tripolsky <tripolskypetr@gmail.com>
2025-01-22 01:19:44 +0500

    Update params.ts

--- src/config/params.ts
+++ src/config/params.ts
@@ -3,7 +3,7 @@ export const CC_MONGO_CONNECTION_STRING = process.env.CC_MONGO_CONNECTION_STRING
 export const CC_OLLAMA_HOST = process.env.CC_OLLAMA_HOST || "http://127.0.0.1:11434";
 export const CC_OLLAMA_CHAT_MODEL = process.env.CC_OLLAMA_CHAT_MODEL || "nemotron-mini:4b";
 export const CC_OLLAMA_EMBEDDER_MODEL = process.env.CC_OLLAMA_EMBEDDER_MODEL || "nomic-embed-text";
-export const CC_OLLAMA_MESSAGES = 15;
+export const CC_OLLAMA_MESSAGES = 5;
 
 export const CC_WWWROOT_PORT = parseInt(process.env.CC_WWWROOT_PORT) || 80;
 export const CC_EXECUTE_TEST = !!process.env.CC_EXECUTE_TEST;
